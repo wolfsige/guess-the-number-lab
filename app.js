3a7eb10a58e5c08b2bba1aa22f0aa516ce3eb887
Wolf <d.wolftripp@gmail.com>
2022-01-29 19:37:05 -0800

    add getGuess

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('party')
+
+
+
+let prevGuess = []
+
+const game = {
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+
+  play: function() {
+    if (getGuess = true){
+
+      guess.push(prevGuess)
+
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    } else {
+      
+    }
+  }
+}
+
+  let getGuess = function(guess){
+      result = window.prompt(`Enter a guess between ${smallestNum} and ${biggestNum}`)
+
+    do {
+      if(guess > secretNum){
+        prevGuess.push(guess)
+        return guess + ' was too high. Here is what youve guessed so far ' + prevGuess
+        
+      } else {
+        prevGuess.push(guess)
+        return guess + ' was too low. Here is what youve guessed so far ' + prevGuess
+      }
+      
+       
+    } while (guess !== secretNum);
+  }
+
+  
