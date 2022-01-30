4a8814dd5ee8d847442a277b38d4873f23d7082a
Wolf <d.wolftripp@gmail.com>
2022-01-30 14:53:00 -0800

    finish Lab

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// console.log("here")
 
 
 const game = {
@@ -9,49 +9,67 @@ const game = {
   prevGuesses: [],
 
   play: function() {
+    // console.log(this.title);
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    console.log(this.secretNum)
-    while (this.getGuess() !== this.secretNum) {
+    // console.log(this.secretNum)
+    
+    do {
+      this.getGuess()
+      this.render()
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
       
-      if (this.getGuess() === NaN){
-        prompt (`Please guess a NUMBER!`)
-      } else {
-        this.getGuess()
-      }
-        this.render()
+    
       
-      
-      
-    }
     
   },
   getGuess: function(){
-    let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}!`)
-    guess = parseInt(guess)
-    console.log(guess)
+    let guess;
     
-    let valid = null
     do {
-      if(guess >= this.biggestNum || guess <= this.smallestNum){
-        
-      } 
-    } while (valid === false)
+      
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}!`))
+
+      // console.log(guess)
+
+    } while (guess > this.biggestNum || guess < this.smallestNum || isNaN(guess))
     
     this.prevGuesses.push(guess)
-    console.log(`Here are your previous guesses: ${game.prevGuesses}`)
+    return `${guess} Here are your previous guesses: ${game.prevGuesses}`
+    
     
 
   },
   render: function(){
-    let daGuess = this.getGuess()
-    if ( daGuess === this.secretNum){
-      console.log(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
-    } else if (daGuess > this.secretNum){
-      console.log(`Your guess is too high! Previous guesses ${this.prevGuesses}`)
-    } else (daGuess < this.secretNum) 
-      console.log(`Your guess is too low! Previous guesses ${this.prevGuesses}`)
+
+    // let daGuess = this.getGuess()
+    // console.log(this.getGuess())
+    let msg;
+
+    // if (daGuess === this.secretNum){
+    //   return `Congrats! You guessed the number in ${this.prevGuesses.length}!`
+    // } else if (daGuess > this.secretNum){
+    //   return `Your guess is too high! Previous guesses ${this.prevGuesses}`
+    // } else (daGuess < this.secretNum) 
+    //   return `Your guess is too low! Previous guesses ${this.prevGuesses}`
     
+    if (this.prevGuesses[this.prevGuesses.length - 1] > this.secretNum) {
+      msg = `Your guess is too high! Previous guesses ${this.prevGuesses}`
+      // console.log(this.prevGuesses[this.prevGuesses.length - 1])
+    } 
+    else if (this.prevGuesses[this.prevGuesses.length - 1] < this.secretNum){
+      msg = `Your guess is too low! Previous guesses ${this.prevGuesses}`
+      // console.log(this.prevGuesses[this.prevGuesses.length - 1])
+    } else if (this.prevGuesses[this.prevGuesses.length - 1] === this.secretNum){
+      msg = `Congrats! You guessed the number in ${this.prevGuesses.length}!`
+      // console.log(this.prevGuesses, this.prevGuesses[this.prevGuesses.length - 1])
+    } else {
+      console.log('yes')
+    }
+    
+    
+    alert(msg);
+
   }
 }
 
