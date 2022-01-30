ae3d79d32e724ed144732accac60977fad87211e
Wolf <d.wolftripp@gmail.com>
2022-01-30 12:40:20 -0800

    render added

--- app.js
+++ app.js
@@ -12,23 +12,47 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     console.log(this.secretNum)
+    while (this.getGuess() !== this.secretNum) {
+      
+      if (this.getGuess() === NaN){
+        prompt (`Please guess a NUMBER!`)
+      } else {
+        this.getGuess()
+      }
+        this.render()
+      
+      
+      
+    }
+    
   },
   getGuess: function(){
     let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}!`)
     guess = parseInt(guess)
     console.log(guess)
     
-    let valid = 
+    let valid = null
     do {
-      
-      //do this while guess is invalid
       if(guess >= this.biggestNum || guess <= this.smallestNum){
         
-        console.log('error')
-      }
-    } while ()
+      } 
+    } while (valid === false)
+    
+    this.prevGuesses.push(guess)
+    console.log(`Here are your previous guesses: ${game.prevGuesses}`)
+    
+
   },
   render: function(){
-    // code to render (you'll get to this later in the lab)
+    let daGuess = this.getGuess()
+    if ( daGuess === this.secretNum){
+      console.log(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
+    } else if (daGuess > this.secretNum){
+      console.log(`Your guess is too high! Previous guesses ${this.prevGuesses}`)
+    } else (daGuess < this.secretNum) 
+      console.log(`Your guess is too low! Previous guesses ${this.prevGuesses}`)
+    
   }
 }
+
+game.play()
